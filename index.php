fbdc9e5b4ecb18d0a9475815790ca3544a3d32ff
RobyLedda94 <robyledda01@yahoo.it>
2024-09-18 14:31:31 +0200

    [UPD] definita la categoria animals e il costrutto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+// definisco la classe per la cadegoria degli animali
+class Animals {
+    public $name;
+
+    // definisco il costrutto per la classe Animals
+    public function __construct($name) {
+        $this->name = $_name;
+    }
+}
+
+?>
